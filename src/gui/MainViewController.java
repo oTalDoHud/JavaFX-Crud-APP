8b75acb2b902d4513062fb89b46838bb6626dabc
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-12-28 19:33:58 -0300

    Sobre View e mudança de tela

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,13 +1,24 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alerts;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
+	
+
 
 	@FXML
 	private MenuItem menuItemVendedor;
@@ -30,12 +41,37 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemSobreAction() {
-		System.out.println("Sobre");
+		loadView("/gui/Sobre.fxml");
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		
 		
+	}
+	
+	private synchronized void loadView (String novaTela) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().
+					getResource(novaTela));
+			VBox newVBox = loader.load();
+			
+			Scene mainCene = Main.getMainScene();
+			
+			VBox mainVBox = (VBox) ((ScrollPane) mainCene.getRoot())
+					.getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			mainVBox.getChildren().clear();
+			
+			mainVBox.getChildren().add(mainMenu);
+			
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Erro ao carregar a página"
+					, e.getMessage(), AlertType.ERROR);
+		}
 	}
 }
