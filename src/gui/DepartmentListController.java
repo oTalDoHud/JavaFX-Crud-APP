47dba244069a9ebaf2da3edeaa79a7a95210657e
oTalDoHud <hudson.lucas.vieira@gmail.com>
2021-12-28 20:46:05 -0300

    Department service

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,9 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable{
 
+	private DepartmentService service;
+	
 	@FXML
 	private TableView<Department> tableViewDepartments;
 	
@@ -28,8 +34,15 @@ public class DepartmentListController implements Initializable{
 	private Button btNovo;
 	
 	@FXML
+	private ObservableList<Department> obsList;
+	
+	@FXML
 	public void onBtNovoAction() {
 		System.out.println("Novo");
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@Override
@@ -45,6 +58,19 @@ public class DepartmentListController implements Initializable{
 		
 		tableViewDepartments
 			.prefHeightProperty().bind(stage.heightProperty());
+
+	}
+	
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Serviço está nulo");
+		}
+		
+		List<Department> list = service.findAll();
+		
+		obsList = FXCollections.observableArrayList(list);
+		
+		tableViewDepartments.setItems(obsList);
 
 	}
 
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable{
 	
@@ -36,7 +37,7 @@ public class MainViewController implements Initializable{
 	
 	@FXML
 	public void onMenuItemDepartamentoAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView2("/gui/DepartmentList.fxml");
 	}
 	
 	@FXML
@@ -68,6 +69,37 @@ public class MainViewController implements Initializable{
 			mainVBox.getChildren().add(mainMenu);
 			
 			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Erro ao carregar a página"
+					, e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
+	private synchronized void loadView2 (String novaTela) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().
+					getResource(novaTela));
+			VBox newVBox = loader.load();
+			
+			Scene mainCene = Main.getMainScene();
+			
+			VBox mainVBox = (VBox) ((ScrollPane) mainCene.getRoot())
+					.getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			mainVBox.getChildren().clear();
+			
+			mainVBox.getChildren().add(mainMenu);
+			
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			DepartmentListController controller = loader
+					.getController();
+			
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
 			
 		} catch (IOException e) {
 			Alerts.showAlert("IOException", "Erro ao carregar a página"
